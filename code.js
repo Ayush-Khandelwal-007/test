612e1379f786d78cdcfb633107f806c841bfdc9f
Ayush Khandelwal <59086855+Ayush-Khandelwal-007@users.noreply.github.com>
2020-04-30 16:34:34 +0530

    Add files via upload
    code.js

---
+++ code.js
@@ -0,0 +1,51 @@
+document.getElementById("bt").onclick=function(){
+    var c=0;
+    var t=document.getElementById("1");
+    if(t)
+    {
+        c+=1;
+    }
+    var t=document.getElementById("2");
+    if(t)
+    {
+        c+=1;
+    }
+    var t=document.getElementById("3");
+    if(t)
+    {
+        c+=1;
+    }
+    var t=document.getElementById("4");
+    if(t)
+    {
+        c+=1;
+    }
+    var t=document.getElementById("5");
+    if(t)
+    {
+        c+=1;
+    }
+    var t=document.getElementById("6");
+    if(t)
+    {
+        c+=1;
+    }
+    var t=document.getElementById("7");
+    if(t)
+    {
+        c+=1;
+    }
+
+    var x=0;
+    var y=0;
+    var z=0;
+    if(c>=4){
+        x=1;
+    }
+    y=document.getElementById("8");
+    z=document.getElementById("9");
+    if(x+y+z==0)
+    {
+        document.getElementById("mess").innerHTML="<p>Cdfchchgtvf</p>"
+    }
+}
