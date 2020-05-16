15912a7c26b755a2454add673614eb9796669533
Ayush Khandelwal <ayush@Ayushs-MacBook-Air.local>
2020-05-16 17:05:05 +0530

    naya ho gaya

--- code.js
+++ code.js
@@ -56,19 +56,19 @@ function pageReady() {
         }
         if(x+y+z==0)
         {
-            document.getElementById("mess").innerHTML="<p>You are totally fine.</p>"
+            document.getElementById("dialouge").innerHTML="<p>You are totally fine.</p>"
         }
         if(x+y+z==1)
         {
-            document.getElementById("mess").innerHTML="<p>Maintain social distancing.</p>"
+            document.getElementById("dialouge").innerHTML="<p>Maintain social distancing.</p>"
         }
         if(x+y+z==2)
         {
-            document.getElementById("mess").innerHTML="<p>You should go on quarantine.</p>"
+            document.getElementById("dialouge").innerHTML="<p>You should go on quarantine.</p>"
         }
         if(x+y+z==3)
         {
-            document.getElementById("mess").innerHTML="<p>Contant to doctor as soon as possible</p>"
+            document.getElementById("dialouge").innerHTML="<p>Contant to doctor as soon as possible</p>"
         }
     }
 }
